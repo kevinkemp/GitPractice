50a2019bf580ab1d21d524ef12820de290dbd02d
Kevin Kemp <kmkemp11@gmail.com>
2012-11-30 16:14:07 -0600

    commits 1-3 combined

--- GitPractice/GitPractice/Class1.cs
+++ GitPractice/GitPractice/Class1.cs
@@ -8,5 +8,11 @@ namespace GitPractice
 {
     public class Class1
     {
+        public void Method1(){
+            
+        }
+        public void Method2() {
+            var x = 2;
+        }
     }
 }
